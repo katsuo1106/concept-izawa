4f386c3ff8dd2091f59e1e0d93ddf40a2083ef90
katsuo1106 <katsuo.so-young@hotmail.co.jp>
2022-08-13 20:58:32 +0900

    パンケーキにデータ入るのを確認

--- app/controllers/wires_controller.rb
+++ app/controllers/wires_controller.rb
@@ -4,18 +4,25 @@ class WiresController < ApplicationController
   end
   
   def index
+    @wire = Wire.all
   end
 
   def new
     @wire = Wire.new
   end
-  
+
   def create
-    @wire = Wire.new
+    @wire = Wire.create(wire_params)
     if @wire.save
       wires_path
     else
       render :new
     end
+  end
+
+  private
+
+  def wire_params
+    params.require(:wire).permit(:hinban, :kei_id ,:iro_id ,:nagasa, :tanka)
   end
 end
