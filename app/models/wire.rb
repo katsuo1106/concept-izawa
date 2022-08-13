f9354a581666ab2b92df0e0b84888a71302fa58b
katsuo1106 <katsuo.so-young@hotmail.co.jp>
2022-08-13 19:28:37 +0900

    アソシエーション

--- app/models/wire.rb
+++ app/models/wire.rb
@@ -1,2 +1,11 @@
 class Wire < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :kei
+  belongs_to :iro
+
+  validates :hinban, presence: true
+  validates :kei_id, presence: true, numericality: { other_than: 1 }
+  validates :iro_id, presence: true, numericality: { other_than: 1 }
+  validates :nagasa, presence: true
+  validates :tanka, presence: true
 end
